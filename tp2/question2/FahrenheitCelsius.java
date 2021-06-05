3ccfd5abd0a3671696055fd41ae7070cbaeee772
raghed idris <raghed.idriss@isae.edu.lb>
2021-06-06 01:13:15 +0300

    question 3 done

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -16,7 +16,11 @@ public class FahrenheitCelsius{
       */
      public static void main(String[] args){
        try{
-       
+       for (int i=0;i<args.length;i++){
+             int fahrenheit = Integer.parseInt(args[i]);
+            float celsius = fahrenheitEnCelsius(fahrenheit);
+            System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); 
+        }
       
        }catch(NumberFormatException nfe){
            System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
@@ -30,8 +34,10 @@ public class FahrenheitCelsius{
       *   @return  la conversion en degré Celsius
       */
      public static float fahrenheitEnCelsius( int f){
-       // ...
-       return 0.F;	// à compléter	en remplaçant la valeur retournée par la fonction de conversion
+         float c= ((float) (f-32)*5/9);
+       return (float)  ((int)(c*10)/10.0);
+       
+       //return 0.F;    // à compléter    en remplaçant la valeur retournée par la fonction de conversion
        // ...
      }
 
--- tp2/question2/FahrenheitCelsiusTest.java
+++ tp2/question2/FahrenheitCelsiusTest.java
@@ -59,6 +59,14 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
     protected void tearDown() // throws java.lang.Exception
     {
         //Libérez ici les ressources engagées par setUp()
+                assertEquals("    0 °F -> -17.7 °C ? ", -17.7, question1.FahrenheitCelsius.fahrenheitEnCelsius(0), 0.1);
+        assertEquals("  100 °F -> 37.7 °C ? ", 37.7, question1.FahrenheitCelsius.fahrenheitEnCelsius(100), 0.1);
+        assertEquals(" 2000 °F -> 1093.3 °C ?", 1093.3, question1.FahrenheitCelsius.fahrenheitEnCelsius(2000), 0.1);
+        assertEquals("   54 °F -> 12.2 °C ?", 12.2, question1.FahrenheitCelsius.fahrenheitEnCelsius(54), 0.1);
+        //extra tests
+       // assertEquals("   ZZ °F -> error °C ?", exception.getMessage() , question1.FahrenheitCelsius.fahrenheitEnCelsius("ZZ"), 0.1);
+        assertEquals("   250 °F -> 121.1 °C ?", 121.1, question1.FahrenheitCelsius.fahrenheitEnCelsius(250), 0.1);
+        assertEquals("   212 °F -> 100 °C ?", 100, question1.FahrenheitCelsius.fahrenheitEnCelsius(212), 0.1);
     }
 
     /**
